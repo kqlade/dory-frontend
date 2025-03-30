6ca5a81069aa861335e98bffa7ecdd4657ece3e8
kqlade <147005128+kqlade@users.noreply.github.com>
2025-03-30 15:05:51 -0700

    Remove unused type declaration files

--- src/types/wink-bm25-text-search.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'wink-bm25-text-search' {
-  export interface BM25Instance {
-    init: (corpus: string[][]) => void;
-    search: (query: string[]) => number[];
-  }
-
-  export function BM25(): BM25Instance;
-} 
